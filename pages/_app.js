aa16ec9ba36be889c185fce78d1fc526cdd42e25
Cryptic7 <112565113+Cryptic7X@users.noreply.github.com>
2026-03-28 19:28:15 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import '../styles/globals.css';
+import Footer from '../components/Footer';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  // Pages where footer should NOT show
+  const noFooter = router.pathname.startsWith('/[mediaType]');
+
+  return (
+    <>
+      <Component {...pageProps} />
+      {!noFooter && <Footer />}
+    </>
+  );
 }
-
-
-
-
