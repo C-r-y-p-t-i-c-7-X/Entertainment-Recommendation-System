7710363e40017dbe927abf80d33c3b576ec563a8
Cryptic7 <112565113+Cryptic7X@users.noreply.github.com>
2026-03-28 14:50:58 +0530

    Create credits.js

---
+++ pages/api/credits.js
@@ -0,0 +1,9 @@
+export default async function handler(req, res) {
+  const { id, type } = req.query;
+  const response = await fetch(
+    `https://api.themoviedb.org/3/${type}/${id}/credits?language=en-US`,
+    { headers: { Authorization: `Bearer ${process.env.TMDB_BEARER_TOKEN}` } }
+  );
+  const data = await response.json();
+  res.status(200).json(data);
+}
